6aec85df18d492ac8788f6f25ea04e52db2d27db
Sai Vardhan Kadambala <85542869+vardhankadambala@users.noreply.github.com>
2023-05-16 18:21:28 +0530

    java program

---
+++ BMI/src/bmi/BMI.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package bmi;
+
+/**
+ *
+ * @author HP
+ */
+public class BMI {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
